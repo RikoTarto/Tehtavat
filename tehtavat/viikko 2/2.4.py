7bc0860f0391e16924dc402f6ffef0d5cf9c8bb1
rikotarto <riko.tarto@metropolia.fi>
2026-02-10 18:51:42 +0200

    viikko 1.1

---
+++ tehtavat/viikko 2/2.4.py
@@ -0,0 +1,13 @@
+print("Anna minulle kolme kokonaislukua")
+
+kokonaisluku1= int(input("numero1: "))
+kokonaisluku2= int(input("numero2: "))
+kokonaisluku3= int(input("numero3: "))
+
+summa= kokonaisluku1 + kokonaisluku2 + kokonaisluku3
+tulo= kokonaisluku1 * kokonaisluku2 * kokonaisluku3
+keskiarvo= (kokonaisluku1 + kokonaisluku2 + kokonaisluku3) / 3
+
+print("Näiden lukujen summa on:", summa)
+print("Näiden lukujen tulo on:", tulo)
+print("Näiden lukujen keskiarvo on:", keskiarvo)
