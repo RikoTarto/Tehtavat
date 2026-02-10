6e8723e70dfc6ee4793e413aed03f91dc5a74981
rikotarto <riko.tarto@metropolia.fi>
2026-02-10 18:31:02 +0200

    viikko 1.1

---
+++ tehtavat/viikko 2/2.3.py
@@ -0,0 +1,9 @@
+import math
+
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna myös korkeus: "))
+
+piiri= kanta*2 + korkeus*2
+print ("Piiri on", piiri)
+pinta_ala= kanta * korkeus
+print ("Pinta-ala on", pinta_ala)
